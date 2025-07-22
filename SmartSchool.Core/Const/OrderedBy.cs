4f188b9e24e145942c623c85e1819f36ca81ab24
musab-program <musabalnagri@gmail.com>
2025-07-22 17:55:29 +0300

    Info for system in program.cs

---
+++ SmartSchool.Core/Const/OrderedBy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Core.Const
+{
+    /// <summary>
+    /// This class identifinng the constant variable for ordering
+    /// </summary>
+    public static class OrderedBy
+    {
+        public const string Ascending = "ASD";
+        public const string Descending = "DESD";
+
+    }
+}
---
+++ SmartSchool.EF/ImplementedClasses/BaseReporstery.cs
@@ -0,0 +1,19 @@
+﻿using SmartSchool.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.EF.ImplementedClasses
+{
+    public class BaseReporstery<T> : IBaseReporstery<T> where T : class
+    {
+        protected ApplicationDbContext _context;
+
+        public BaseReporstery(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
