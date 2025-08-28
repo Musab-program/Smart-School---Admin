1e78cd3b997c3bc2a3dd20abdc2e56b3fb570005
Musab <musabalnagri@gmail.com>
2025-08-28 16:01:06 +0300

    أصفا ترانسكشن الطالب للعميليات

--- SmartSchool.Main/Dtos/StudentDto.cs
+++ SmartSchool.Main/Dtos/StudentDto.cs
@@ -2,32 +2,27 @@
 {
     public class StudentDto
     {
-            // Student Properties
-            public int? Id { get; set; } // قد يكون null عند الإضافة الجديدة
-            public DateTime RegisterDate { get; set; }
+        // Student Properties
+        public int? Id { get; set; } // قد يكون null عند الإضافة الجديدة
+        public DateTime RegisterDate { get; set; }
 
         // Users Properties
-            public int UserId { get; set; } 
-            public string UserName { get; set; } 
-            public string UserEmail { get; set; } 
-            public byte[] UserPassword { get; set; }
-            public string UserPhone { get; set; }
-            public DateTime UserDateOfBirth { get; set; }
-            public DateTime UserCreatedAt { get; set; }
-            public bool IsActiveUser { get; set; }
-            public string UserAddress { get; set; }
-            public string UserGender { get; set; }
-            public int RoleId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public string UserPhone { get; set; }
+        public DateTime UserDateOfBirth { get; set; }
+        public bool IsActiveUser { get; set; }
+        public string UserAddress { get; set; }
+        public string UserGender { get; set; }
+        public int RoleId { get; set; }
 
-            // Guardian Properties
-            public int GuardianId { get; set; } 
-            public string GuardianName { get; set; }
-            public int GuardianUserId { get; set; } //*******************
-            public int RelationTypeId { get; set; } 
-            public string SecondryPhone { get; set; }
+        // Guardian Properties
+        public int GuardianId { get; set; }
 
         //Group Properties
-        public int GroupId { get; set; } 
-        }
+        public int GroupId { get; set; }
+    }
 
 }
